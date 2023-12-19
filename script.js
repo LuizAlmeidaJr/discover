6434510796a90f4737994c7fc2d83bfd2e874a69
Luiz Almeida Jr <92612908+LuizAlmeidaJr@users.noreply.github.com>
2023-12-19 15:13:30 -0300

    fix profile image
    Correção da alteração da imagem de perfil nos modos light e dark.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const html = document.documentElement
+html.classList.remove("light")
+
 function toggleMode() {
-  const html = document.documentElement
   const profile = document.getElementById("profileImage")
 
   html.classList.toggle("light")
-  if(html.classList.contains("light")){
+  if (html.classList.contains("light")) {
     profile.setAttribute("src", "./assets/assets/avatar-light.png")
   } else {
     profile.setAttribute("src", "./assets/assets/avatar.png")
